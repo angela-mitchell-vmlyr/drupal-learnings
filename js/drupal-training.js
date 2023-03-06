ea892fdab2ccac377f21e599bed3a9d348d52dd2
Angela Mitchell <angela.mitchell@vmlyr.com>
2023-03-06 11:28:17 -0600

    initial commit

---
+++ js/drupal-training.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Drupal Training behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalTraining = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
